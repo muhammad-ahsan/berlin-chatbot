ad28606733a0203e5dcd90d14955529b712f629d
ahsan <muhammad.ahsan@gmail.com>
2019-03-23 21:16:52 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='berlin-chatbot',
+    version='1.0',
+    packages=['berlin-chatbot', ],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
