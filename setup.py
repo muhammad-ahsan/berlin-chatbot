f98aec4ee918923d18f94c2dcf3bc4cb491b47ee
ahsan <muhammad.ahsan@gmail.com>
2019-03-24 22:09:13 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='berlin-chatbot',
     version='1.0',
-    packages=['berlinchatbot', ],
+    packages=find_packages(),
     license='MIT License',
     long_description=open('README.md').read(),
 )
